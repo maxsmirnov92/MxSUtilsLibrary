65875c2147d51d55b0ac2dc29e5deec7da500950
maxsmirnov92 <maxsim.v.smirnov@altarix.ru>
2016-07-27 15:16:52 +0400

    1.0.2

---
+++ commonutils/src/main/java/net/maxsmr/commonutils/data/gson/PropertyBasedAdapter.java
@@ -0,0 +1,44 @@
+package net.maxsmr.commonutils.data.gson;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+
+public class PropertyBasedAdapter<T> implements JsonSerializer<T>, JsonDeserializer<T> {
+
+//    @NonNull
+//    private final Class<T> mClass;
+//
+//    public PropertyBasedAdapter(@NonNull Class<T> clazz) {
+//        mClass = clazz;
+//    }
+
+    private static final String CLASS_META_KEY = "CLASS_META_KEY";
+
+    @Override
+    public JsonElement serialize(T src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonElement jsonElem = context.serialize(src, src.getClass());
+        jsonElem.getAsJsonObject().addProperty(CLASS_META_KEY,
+                src.getClass().getCanonicalName());
+        return jsonElem;
+    }
+
+    @Override
+    public T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObj = json.getAsJsonObject();
+        String className = jsonObj.get(CLASS_META_KEY).getAsString();
+        try {
+            Class<?> clz = Class.forName(className);
+            return context.deserialize(json, clz);
+        } catch (ClassNotFoundException e) {
+            throw new JsonParseException(e);
+        }
+    }
+
+}
