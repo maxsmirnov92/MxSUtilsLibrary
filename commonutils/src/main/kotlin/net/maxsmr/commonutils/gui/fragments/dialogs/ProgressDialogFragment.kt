f15a437bb9bb3ffcda43b063f02dc4e3c3d26399
maxsmirnov92 <max.smirnov92@yandex.ru>
2021-07-08 23:25:09 +0400

    TypedDialogFragment minor fixes, libs versions up

--- commonutils/src/main/kotlin/net/maxsmr/commonutils/gui/fragments/dialogs/ProgressDialogFragment.kt
+++ commonutils/src/main/kotlin/net/maxsmr/commonutils/gui/fragments/dialogs/ProgressDialogFragment.kt
@@ -1,14 +1,20 @@
 package net.maxsmr.commonutils.gui.fragments.dialogs
 
+import android.app.Dialog
+import android.app.ProgressDialog
 import android.content.Context
 import android.os.Bundle
+import android.view.*
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AlertDialog
 
 const val ARG_PROGRESS_BAR_ID = "BaseProgressTypedDialogFragment#ARG_PROGRESS_BAR_ID"
 const val ARG_LOADING_MESSAGE_VIEW_ID = "BaseProgressTypedDialogFragment#ARG_LOADING_MESSAGE_VIEW_ID"
+const val ARG_IS_ALERT = "AlertDialogFragment#ARG_IS_ALERT"
+const val ARG_CONTAINER_ID = "AlertDialogFragment#ARG_CONTAINER_ID"
 
 open class ProgressDialogFragment : AlertTypedDialogFragment() {
 
@@ -18,35 +24,76 @@ open class ProgressDialogFragment : AlertTypedDialogFragment() {
     protected var loadingMessageView: TextView? = null
         private set
 
-    override fun onDialogCreated(dialog: AlertDialog) {
-        super.onDialogCreated(dialog)
-        customView?.let {
-            val progressBarId: Int = args.getInt(ARG_PROGRESS_BAR_ID)
-            if (progressBarId != 0) {
-                progressBar = it.findViewById(progressBarId)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return if (args.getBoolean(ARG_IS_ALERT, true)) {
+            super.onCreateDialog(savedInstanceState) as AlertDialog
+        } else {
+            ProgressDialog(requireContext(),
+                    args.getString(ARG_TITLE),
+                    args.getInt(ARG_CUSTOM_VIEW_RES_ID),
+                    shouldBeCancelable())
+        }
+    }
+
+    override fun setupDialog(dialog: Dialog) {
+        super.setupDialog(dialog)
+        if (dialog is AlertDialog) {
+
+            // FIXME wrap_content при использовании AlertDialog
+            dialog.setCanceledOnTouchOutside(isCancelable)
+//        dialog.setContentView(layoutRes)
+            dialog.window?.let {
+                val containerId = args.getInt(ARG_CONTAINER_ID)
+//            it.setContentView(layoutRes)
+                if (containerId != 0) {
+                    val dialogContainer = it.findViewById(containerId) as View
+                    dialogContainer.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                        override fun onGlobalLayout() {
+                            it.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                            dialogContainer.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                        }
+                    })
+//                forceLayoutParams(dialogContainer, WRAP_CONTENT, WRAP_CONTENT)
+                } else {
+                    it.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    it.setGravity(Gravity.CENTER)
+                }
             }
-            val loadingMessageId: Int = args.getInt(ARG_LOADING_MESSAGE_VIEW_ID)
-            if (loadingMessageId != 0) {
-                with(it.findViewById<TextView>(loadingMessageId)) {
-                    if (this != null) {
-                        val message = args.getString(ARG_MESSAGE)
-                        text = message
+
+            customView?.let {
+                val progressBarId: Int = args.getInt(ARG_PROGRESS_BAR_ID)
+                if (progressBarId != 0) {
+                    progressBar = it.findViewById(progressBarId)
+                }
+                val loadingMessageId: Int = args.getInt(ARG_LOADING_MESSAGE_VIEW_ID)
+                if (loadingMessageId != 0) {
+                    with(it.findViewById<TextView>(loadingMessageId)) {
+                        if (this != null) {
+                            val message = args.getString(ARG_MESSAGE)
+                            text = message
+                        }
+                        loadingMessageView = this
                     }
-                    loadingMessageView = this
                 }
             }
         }
     }
 
-    abstract class BaseBuilder<F: ProgressDialogFragment> @JvmOverloads constructor(
+    abstract class BaseProgressBuilder<F : ProgressDialogFragment> @JvmOverloads constructor(
+            private val isAlert: Boolean = false,
+            @IdRes
+            private val containerId: Int = 0,
             @IdRes
             protected val progressBarId: Int = 0,
             @IdRes
             protected val loadingMessageViewId: Int = 0
-    ) : BaseTypedDialogFragment.Builder<AlertDialog, F>() {
+    ) : BaseTypedDialogFragment.Builder<Dialog, F>() {
 
         override fun createArgs(context: Context): Bundle {
             return super.createArgs(context).apply {
+                if (containerId != 0) {
+                    putInt(ARG_CONTAINER_ID, containerId)
+                }
                 if (progressBarId != 0) {
                     putInt(ARG_PROGRESS_BAR_ID, progressBarId)
                 }
@@ -57,9 +104,28 @@ open class ProgressDialogFragment : AlertTypedDialogFragment() {
         }
     }
 
-    class Builder: BaseBuilder<ProgressDialogFragment>() {
+    class ProgressBuilder : BaseProgressBuilder<ProgressDialogFragment>() {
 
         override fun build(context: Context) = newInstance(createArgs(context))
+    }
+
+    private class ProgressDialog(
+            context: Context,
+            title: String?,
+            @LayoutRes contentViewResId: Int?,
+            cancelable: Boolean
+    ) : Dialog(context) {
+
+        init {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setTitle(title)
+            setCancelable(cancelable)
+            setCanceledOnTouchOutside(cancelable)
+            setOnCancelListener(null)
+            contentViewResId?.takeIf { it != 0 }?.let {
+                setContentView(it)
+            }
+        }
     }
 
     companion object {
