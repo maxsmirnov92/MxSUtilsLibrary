c88c02881cfb32a0e1911fd140efb398654ae0f8
maxsmirnov92 <max.smirnov92@yandex.ru>
2024-03-28 17:32:13 +0400

    TalkbackUtils methods

--- commonutils/src/main/java/net/maxsmr/commonutils/hardware/TalkbackUtils.kt
+++ commonutils/src/main/java/net/maxsmr/commonutils/hardware/TalkbackUtils.kt
@@ -1,15 +1,33 @@
 package net.maxsmr.commonutils.hardware
 
-import android.content.Context
+import android.view.View
 import android.view.accessibility.AccessibilityEvent
-import android.view.accessibility.AccessibilityManager
+import android.view.accessibility.AccessibilityNodeInfo
 
-fun Context.sendAnnouncementEvent(text: String?) {
-    if (text.isNullOrEmpty()) return
-    val accessibilityEvent = AccessibilityEvent.obtain()
-    accessibilityEvent.eventType = AccessibilityEvent.TYPE_ANNOUNCEMENT
-    accessibilityEvent.text.add(text)
-    val manager = getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager? ?: return
-    if (!manager.isEnabled) return
-    manager.sendAccessibilityEvent(accessibilityEvent)
+@JvmOverloads
+fun View.requestAccessibilityFocus(shouldSelect: Boolean = true): View {
+    performAccessibilityAction(AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS, null)
+    if (shouldSelect) {
+        sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED)
+    }
+    return this
+}
+
+/**
+ * Устанавливает фокус без озвучки
+ */
+fun View.requestFocusWithoutAnnounce() {
+    val isImportantForAccessibility = importantForAccessibility
+    post {
+        importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
+        // запрашиваем фокус
+        requestAccessibilityFocus(false)
+        accessibilityDelegate = object : View.AccessibilityDelegate() {
+            override fun sendAccessibilityEvent(host: View, eventType: Int) {
+                super.sendAccessibilityEvent(host, eventType)
+                importantForAccessibility = isImportantForAccessibility
+                accessibilityDelegate = null
+            }
+        }
+    }
 }
